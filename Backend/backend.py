6e14423b5a603b4cf079517ec67269548ed0a477
Hamza-Tahirr <hamzatahiramin2@gmail.com>
2024-02-26 00:53:47 +0500

    init be

--- Backend/backend.py
+++ Backend/backend.py
@@ -7,17 +7,18 @@ from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 import os
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
+import pinecone
 
 # Loading environment variables
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'YourAPIKey')
 
 # Load data
-loader = TextLoader(file_path="yahan File path dalna ./ something.pdf")
+loader = TextLoader(file_path="../data/PaulGrahamEssays/vb.txt")
 data = loader.load()
 
 # Chunking data
@@ -26,10 +27,14 @@ texts = text_splitter.split_documents(data)
 
 # Create embeddings
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-vectorstore = Chroma.from_documents(texts, embeddings)
+
+# Set up Pinecone
+pinecone.init(api_key="YourAPIKey")  # Replace "YourAPIKey" with your actual Pinecone API key
+index_name = "langchaintest"  # put in the name of your Pinecone index here
+vectorstore = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
 
 # Query documents
-query = "What is Artificial Intelligence?"
+query = "What is great about having kids?"
 docs = vectorstore.similarity_search(query)
 
 # Initializing ChatOpenAI and running the chain
